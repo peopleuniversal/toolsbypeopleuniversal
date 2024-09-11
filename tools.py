021f5c6f5f1b5bbe95ee665b788ae3b81e805d37
People Universal <135725113+peopleuniversal@users.noreply.github.com>
2024-09-11 18:46:58 +0700

    Update tools.py
    Signed-off-by: People Universal <135725113+peopleuniversal@users.noreply.github.com>

--- tools.py
+++ tools.py
@@ -1,6 +1,10 @@
+#!bin/python
+
 import os, sys, time
-    
-def menu():
+import random
+
+# main menu
+def menu():    
     os.system("clear")
     time.sleep(4)
     print("\033[0;31m*********************************")
@@ -9,63 +13,88 @@ def menu():
     print("**    Copyright@github.com     **")
     print("*********************************")
     time.sleep(3)
-    print("")
     print("\033[0;32m[1] Sharelock Location")
     print("[2] LITEDDOS")
     print("[3] SpiderBot")
     print("[4] LITETOOLS")
+    print("[5] SpamBotOTP")
     print("[0] Exit")
     time.sleep(2)
-    print("")
+    print("*********************************")
     tools = input("\033[0;30m[ Input Number ] >> ")
+
+    # number 1
     if tools =="1":
         os.system("clear")
         print("\033[0;37mPlease wait for second!")
-        time.sleep(3)
+        time.sleep(2)
         os.system("clear")
-        time.sleep(3)
+        time.sleep(1)
         os.system("git clone https://github.com/sherlock-project/sherlock.git")
-        time.sleep(3)
+        os.system("clear")
+        print("Done All Install...")
+        time.sleep(2)
         os.system("clear")
         os.system("python tools.py")
-    
+
+    # number 2
     if tools =="2":
         os.system("clear")
         os.system("apt update && apt upgrade")
         os.system("pkg install git")
         os.system("pkg install python2")
         os.system("git clone https://github.com/4L13199/LITEDDOS")
-        time.sleep(4)
         os.system("clear")
+        print("Done All Install...")
+        time.sleep(3)
         os.system("python tools.py")
 
+    # number 3
     if tools =="3":
         os.system("clear")
         os.system("apt update && apt upgrade")
         os.system("apt install php")
         os.system("apt install git")
         os.system("git clone https://github.com/Cvar1984/SpiderBot.git")
-        os.system("cd SpiderBot")
         time.sleep(3)
         os.system("clear")
         time.sleep(3)
         print("Tinggal Dijalankan Saja, Seperti Hubunganmu Dengan Dia, Eaaa!:)")
-        time.sleep(2)
+        time.sleep(3)
         print("Contoh Saya Ingin Pakai Botkomen")
-        time.sleep(2)
+        time.sleep(3)
         print("Ketik: php botkoment.php")
-        time.sleep(1)
+        time.sleep(3)
         os.system("clear")
         os.system("python tools.py")
-
+        
+    # number 4
     if tools =="4":
         os.system("clear")
         os.system("pkg update && pkg upgrade")
         os.system("pkg install git")
         os.system("git clone https://github.com/4L13199/LITETOOLS")
         os.system("clear")
+        print("Done All Install...")
+        time.sleep(3)
+        os.system("clear")
+        os.system("python tools.py")
+        
+    # number 5
+    if tools =="5":
+        os.system("clear")
+        print("pls wait for second...")
+        time.sleep(2)
+        os.system("apt update && apt upgrade -y")
+        os.system("apt install python -y")
+        os.system("git clone https://github.com/rickyfazaa/MySPAMBot-OTP")
+        os.system("clear")
+        print("Done All Install....")
+        time.sleep(3)
+        os.system("clear")
         os.system("python tools.py")
 
+    # number exit(0)
     elif tools =="0":
         os.system("clear")
         print("Please Wait 3s...!!!")
@@ -73,7 +102,8 @@ def menu():
         print("\033[0;32mBye Don't Forget Follow Media Social")
         time.sleep(2)
         os.system("exit")
-
+        
+    # command wrong!
     else:
         os.system("clear")
         print("\033[0;31mPls Input Number Valid...!!!")
